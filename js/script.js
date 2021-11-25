48a5f557214b58184b335f3557e64deca4b731ef
alaminjewel <72698595+alaminjewel@users.noreply.github.com>
2021-11-25 19:46:30 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ var rootElement = document.documentElement;
 function handleScroll() {
   // Do something on scroll
   var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
-  if (rootElement.scrollTop / scrollTotal > 0.2) {
+  if (rootElement.scrollTop / scrollTotal > 0.1) {
     // Show button
     scrollToTopBtn.classList.add("showBtn");
   } else {
